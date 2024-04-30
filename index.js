43e5168040618227565fd7e0c093787ffb18910c
tultul <tultulakter963@gmail.com>
2024-04-30 23:11:22 +0600

    added comment

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ async function run() {
     const result = await spotCollection.insertOne(addSpot);
     res.send(result);
    })
-
+// update
    app.put('/spot/:id' , async(req, res) =>{
     const id = req.params.id;
     const filter = {_id: new ObjectId(id)}
