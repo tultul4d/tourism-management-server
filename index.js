89d2d00e7fdc445c8e951f822c3a6c5b87cfbb99
tultul <tultulakter963@gmail.com>
2024-04-30 21:01:41 +0600

    added some changese

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: ["http://localhost:5174", "https://tourism-management-websi-c0728.web.app"],
+    credentials: true,
+};
+app.use(cors(corsConfig))
 app.use(express.json());
 
 
@@ -33,7 +38,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const spotCollection = client.db('spotDB').collection('spot');
@@ -79,8 +84,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
